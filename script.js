52a4783d116820b01b023ef364799974da6c182e
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-07-10 22:34:35 -0700

    script.js updates to set and get items from local storage

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-var today = moment();
-saveBtnEl = $("saveBtn");
+saveBtnEl = $("#9amSaveBtn");
+var nineAM = $("#9am");
 
+var today = moment();
 $("#currentDay").text(today.format("LL"));
 
-
-saveBtnEl.on('click', function () {
-    localStorage.set();
+saveBtnEl.on('click', function (event) {
+    //look up if it should be a 'click' or 'submit' and if 'click' then is preventDefault necessary?
+    event.preventDefault();
+    var calEvent = nineAM.val();
+    if (calEvent == null){
+        console.log("no event to schedule");
+    }
+    else {
+        // console.log(calEvent);
+    localStorage.setItem("9am", calEvent);
+    }
   });
 
-  //upon refresh - localStorage.get
-  //change colors depending on time (setInterval?)
+window.onload = function() {
+    var eventName = localStorage.getItem("9am");
+    if (eventName !== null) nineAM.val(eventName);
+    //change colors depending on time (setInterval?)
+}
