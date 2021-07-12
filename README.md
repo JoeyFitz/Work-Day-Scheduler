1fd7fe3f49ebdaed2618d5fa56fa44a14dd321c5
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-07-11 20:34:09 -0700

    MVP status

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Work-Day-Scheduler
+## Description
+This repo contains source code for a simple work day scheduling web application designed to run locally in your browser. It was a homework assignment with some starter HTML and CSS code provided and designed to reinforce rudimentary employment of jquery. The actual behavior of the application meets the user acceptance criteria included below. In a nutshell, a user loads the application in their browser. The app uses moment.js to render the current date at the top of the application. The user is able to enter text into input fields for each hourly block of the work day. When the user hits the 'save' button to the right of the hourly block, the input value is saved to the browser's local storage. The user can edit the calendar event and overwrite the input value stored locally. Upon refreshing the page, the calendar events are loaded from local storage. There is also some conditional logic that changes the background color of the hourly blocks depending on the current time.
+
+## User Acceptance Criteria
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+
+## Installation
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository.
+
+## Usage
+Visit URL linked below
+
+## Credits
+W3 Schools and Moment.js docs
+
+Screenshot of the deployed application:
+
+
+URL of the deployed application --> https://joeyfitz.github.io/Work-Day-Scheduler/index.html
+
+URL of the GitHub repository --> https://github.com/JoeyFitz/Work-Day-Scheduler
+
--- script.js
+++ script.js
@@ -1,24 +1,145 @@
-saveBtnEl = $("#9amSaveBtn");
-var nineAM = $("#9am");
+//Code feels rather inefficient. Perhaps there is more clever use of arrays, forEach or some other method but couldn't make anything work
 
+//using JQuery to assign variables for each hourly input block
+nine = $("#nine");
+ten = $("#ten");
+eleven = $("#eleven");
+twelve = $("#twelve");
+thirteen = $("#thirteen");
+fourteen = $("#fourteen");
+fifteen = $("#fifteen");
+sixteen = $("#sixteen");
+//using JQuery to assign variables to each save button
+saveNine = $("#saveNine");
+saveTen = $("#saveTen");
+saveEleven = $("#saveEleven");
+saveTwelve = $("#saveTwelve");
+saveThirteen = $("#saveThirteen");
+saveFourteen = $("#saveFourteen");
+saveFifteen = $("#saveFifteen");
+saveSixteen = $("#saveSixteen");
+
+//Uses moment.js to pull current date for display
 var today = moment();
 $("#currentDay").text(today.format("LL"));
 
-saveBtnEl.on('click', function (event) {
-    //look up if it should be a 'click' or 'submit' and if 'click' then is preventDefault necessary?
-    event.preventDefault();
-    var calEvent = nineAM.val();
-    if (calEvent == null){
+//what to execute upon loading the page
+window.onload = function() {
+
+    //Load the schedule blocks from local storage (if any)
+    var loadNine = localStorage.getItem("9am");
+    if (loadNine !== null) nine.val(loadNine);
+    var loadTen = localStorage.getItem("10am");
+    if (loadTen !== null) ten.val(loadTen);
+    var loadEleven = localStorage.getItem("11am");
+    if (loadEleven !== null) eleven.val(loadEleven);
+    var loadTwelve = localStorage.getItem("12pm");
+    if (loadTwelve !== null) twleve.val(loadTwelve);
+    var loadThirteen = localStorage.getItem("1pm");
+    if (loadThirteen !== null) thirteen.val(loadThirteen);
+    var loadFourteen = localStorage.getItem("2pm");
+    if (loadFourteen !== null) fourteen.val(loadFourteen);
+    var loadFifteen = localStorage.getItem("3pm");
+    if (loadFifteen !== null) fifteen.val(loadFifteen);
+    var loadSixteen = localStorage.getItem("4pm");
+    if (loadSixteen !== null) sixteen.val(loadSixteen);
+
+    //Change background color of input block(s) to red if it's the present hour or green if it's a future hour
+    if (today.hour() == 9) { nine.css("background-color","red"); }
+    if (today.hour() < 9) { nine.css("background-color", "green"); }
+    if (today.hour() == 10) { ten.css("background-color","red"); }
+    if (today.hour() < 10) { ten.css("background-color", "green"); }
+    if (today.hour() == 11) { eleven.css("background-color","red"); }
+    if (today.hour() < 11) { eleven.css("background-color", "green"); }
+    if (today.hour() == 12) { twelve.css("background-color","red"); }
+    if (today.hour() < 12) { twelve.css("background-color", "green"); }
+    if (today.hour() == 13) { thirteen.css("background-color","red"); }
+    if (today.hour() < 13) { thirteen.css("background-color", "green"); }
+    if (today.hour() == 14) { fourteen.css("background-color","red"); }
+    if (today.hour() < 14) { fourteen.css("background-color", "green"); }
+    if (today.hour() == 15) { fifteen.css("background-color","red"); }
+    if (today.hour() < 15) { fifteen.css("background-color", "green"); }
+    if (today.hour() == 16) { sixteen.css("background-color","red"); }
+    if (today.hour() < 16) { sixteen.css("background-color", "green"); }
+}
+
+saveNine.on('click', function (event) {
+    var my9oclock = nine.val();
+    if (my9oclock == null){
         console.log("no event to schedule");
     }
     else {
-        // console.log(calEvent);
-    localStorage.setItem("9am", calEvent);
+    localStorage.setItem("9am", my9oclock);
     }
-  });
+})
 
-window.onload = function() {
-    var eventName = localStorage.getItem("9am");
-    if (eventName !== null) nineAM.val(eventName);
-    //change colors depending on time (setInterval?)
-}
+saveTen.on('click', function (event) {
+    var my10oclock = ten.val();
+    if (my10oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("10am", my10oclock);
+    }
+})
+
+saveEleven.on('click', function (event) {
+    var my11oclock = eleven.val();
+    if (my11oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("11am", my11oclock);
+    }
+})
+
+saveTwelve.on('click', function (event) {
+    var my12oclock = twelve.val();
+    if (my12oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("12pm", my12oclock);
+    }
+})
+
+saveThirteen.on('click', function (event) {
+    var my1oclock = thirteen.val();
+    if (my1oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("1pm", my1oclock);
+    }
+})
+
+saveFourteen.on('click', function (event) {
+    var my2oclock = fourteen.val();
+    if (my2oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("2pm", my2oclock);
+    }
+})
+
+saveFifteen.on('click', function (event) {
+    var my3oclock = fifteen.val();
+    if (my3oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("3pm", my3oclock);
+    }
+})
+
+saveSixteen.on('click', function (event) {
+    var my4oclock = sixteen.val();
+    if (my4oclock == null){
+        console.log("no event to schedule");
+    }
+    else {
+    localStorage.setItem("4pm", my4oclock);
+    }
+})
+
